db1cde1857dcdd554785506c738448193b1dd436
Antônio Lima <antonio.student@outlook.com>
2024-08-02 08:09:34 -0300

    Add unit test

--- clinica/tests.py
+++ clinica/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from clinica.models import Contato
+
+
+class ContatoTestCase(TestCase):
+    def test_save(self):
+        contato = Contato.objects.create(prefixo = "12", numero = "34567890")
+        self.assertEqual(contato.id, 1)
